4a173ae727c39de4fc2af3b9cc36ba4c700e4828
nitrodum <nitrodum@yahoo.com>
2023-10-20 15:28:07 -0700

    added DRF and views for Bookings and Menu

--- LittleLemon/reservation/urls.py
+++ LittleLemon/reservation/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from .views import index
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'bookings', views.BookingViewSet)
 
 urlpatterns = [
     path('', index, name='index'),
+    path('', include(router.urls)),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- LittleLemon/reservation/views.py
+++ LittleLemon/reservation/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets, generics
+from django.contrib.auth.models import User
+from .models import Booking, Menu
+from .serializers import UserSerializer, BookingSerializer, MenuSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
